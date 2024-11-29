0c59ac90ada9e6b7729814b56fde6c68aefcdd99
Mishra Ji <harsh@Mishras-MacBook-Air.local>
2024-11-29 08:55:50 +0530

    footer improved

--- src/container/dashboard/Dashboard.jsx
+++ src/container/dashboard/Dashboard.jsx
@@ -1,18 +1,18 @@
-import Slider from './Carousel';
+import Slider from "./Carousel";
 import About from "./About";
 import ProcessSteps from "./ProcessSteps";
-import Navbar from '../../components/common/Navbar';
-import Footer from '../../components/common/Footer'
+import Navbar from "../../components/common/Navbar";
+import Footer from "../../components/common/FooterComponent";
 function Dashboard() {
-    return (
-        <>
-            <Navbar />
-            <Slider />
-            <About />
-            <ProcessSteps />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Slider />
+      <About />
+      <ProcessSteps />
+      <Footer />
+    </>
+  );
 }
 
 export default Dashboard;
