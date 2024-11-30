46694d42cddce01beadfc15ae8b58c55a9c76d39
AnupamBB <anupam@antares-tech.com>
2024-11-30 17:51:10 +0530

    added common header

--- src/container/dashboard/Carousel.jsx
+++ src/container/dashboard/Carousel.jsx
@@ -4,20 +4,24 @@ import slideData from "../../components/json/sliderdata.json";
 import FeatureSection from "../../components/FeatureSection";
 
 const Slider = () => (
-  <div className={styles.sliderContainer}>
-    <Carousel autoplay>
-      {slideData.map((slide, index) => (
-        <div key={index} className={styles.carouselSlide}>
-          <div className={styles.heading}>{slide.heading}</div>
-          <div className={styles.description}>{slide.description}</div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.slideButton}>{slide.buttonText}</button>
-          </div>
-          <FeatureSection />
-        </div>
-      ))}
-    </Carousel>
-  </div>
+    <div className={styles.sliderContainer}>
+        <Carousel autoplay>
+            {slideData.map((slide, index) => (
+                <div key={index} className={styles.carouselSlide}>
+                    <div className={styles.heading}>{slide.heading}</div>
+                    <div className={styles.description}>
+                        {slide.description}
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.slideButton}>
+                            {slide.buttonText}
+                        </button>
+                    </div>
+                    <FeatureSection />
+                </div>
+            ))}
+        </Carousel>
+    </div>
 );
 
 export default Slider;
