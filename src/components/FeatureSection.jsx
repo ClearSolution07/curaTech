aa7968c7b8530caf39fb7f580495d39052e10ecd
Mishra Ji <harsh@Mishras-MacBook-Air.local>
2024-11-30 16:15:02 +0530

    dashboard work started

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Row, Col, Typography } from "antd";
+import {
+  ToolOutlined,
+  ProjectOutlined,
+  BuildOutlined,
+} from "@ant-design/icons";
+
+const { Title, Text } = Typography;
+
+const FeatureSection = () => {
+  const features = [
+    {
+      icon: <ToolOutlined style={{ fontSize: "50px", color: "#c13c3c" }} />,
+      title: "General Contract",
+      description:
+        "Quisque condimentum erat ac orci blandit, in sollicitudin tellus vestibulum",
+    },
+    {
+      icon: <ProjectOutlined style={{ fontSize: "50px", color: "#c13c3c" }} />,
+      title: "Project Planning",
+      description:
+        "Nullam commodo tincidunt nisl, nec vehicula dui interdum nec",
+    },
+    {
+      icon: <BuildOutlined style={{ fontSize: "50px", color: "#c13c3c" }} />,
+      title: "Refurbishment",
+      description:
+        "Sed vitae aliquet ipsum, ut ornare lectus. Proin sed sem risus. Sed nunc",
+    },
+  ];
+
+  return (
+    <Col
+      style={{ padding: "40px 40px", backgroundColor: "white", width: "450px" }}
+    >
+      <Row level={2} style={{ color: "#00215b", textAlign: "left" }}>
+        We Construct and Manage Places and Infrastructures
+      </Row>
+      <Col
+        gutter={[16, 24]}
+        style={{
+          marginTop: "20px",
+        }}
+      >
+        {features.map((feature, index) => (
+          <Row
+            key={index}
+            xs={24}
+            md={8}
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <div>{feature.icon}</div>
+            <div>
+              <div style={{ color: "#00215b", marginTop: "15px" }}>
+                {feature.title}
+              </div>
+              <div style={{ color: "#555" }}>{feature.description}</div>
+            </div>
+          </Row>
+        ))}
+      </Col>
+    </Col>
+  );
+};
+
+export default FeatureSection;
--- src/container/dashboard/Carousel.jsx
+++ src/container/dashboard/Carousel.jsx
@@ -1,25 +1,23 @@
 import { Carousel } from "antd";
 import styles from "./Slider.module.css";
 import slideData from "../../components/json/sliderdata.json";
+import FeatureSection from "../../components/FeatureSection";
 
 const Slider = () => (
-    <div className={styles.sliderContainer}>
-        <Carousel autoplay>
-            {slideData.map((slide, index) => (
-                <div key={index} className={styles.carouselSlide}>
-                    <div className={styles.heading}>{slide.heading}</div>
-                    <div className={styles.description}>
-                        {slide.description}
-                    </div>
-                    <div className={styles.buttonContainer}>
-                        <button className={styles.slideButton}>
-                            {slide.buttonText}
-                        </button>
-                    </div>
-                </div>
-            ))}
-        </Carousel>
-    </div>
+  <div className={styles.sliderContainer}>
+    <Carousel autoplay>
+      {slideData.map((slide, index) => (
+        <div key={index} className={styles.carouselSlide}>
+          <div className={styles.heading}>{slide.heading}</div>
+          <div className={styles.description}>{slide.description}</div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.slideButton}>{slide.buttonText}</button>
+          </div>
+          <FeatureSection />
+        </div>
+      ))}
+    </Carousel>
+  </div>
 );
 
 export default Slider;
