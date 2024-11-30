362903df5d118da8b7de35c0b3518c5b4e2ed304
AnupamBB <anupam@antares-tech.com>
2024-11-30 17:51:10 +0530

    added common header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import Dashboard from './container/dashboard/Dashboard'
-function App() {
+import CommonCarousel from "./components/common/CommonCarousel";
+import slideImage from "./assets/slide.jpg";
+import Dashboard from "./container/dashboard/Dashboard"
 
-	return (
+function App() {
+    return (
         <>
+            {/* <CommonCarousel
+                heading="Discover New Horizons"
+                description="Explore opportunities and possibilities."
+                backgroundImage={slideImage}
+                showFloatingContact={true}
+                containerStyle={{}}
+                floatingContactProps={{
+                    title: "We Construct and Manage Places and Infrastructures",
+                    description: "We offer the best solutions for your needs.",
+                }}
+            /> */}
             <Dashboard />
         </>
     );
 }
 
-export default App
+export default App;
