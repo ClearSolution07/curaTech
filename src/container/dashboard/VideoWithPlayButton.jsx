7dc7c1f2881d7889cab25e3d3c5065ae2fea2f12
Mishra Ji <harsh@Mishras-MacBook-Air.local>
2024-12-02 00:10:24 +0530

    home page done

---
+++ src/container/dashboard/VideoWithPlayButton.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./VideoWithPlayButton.css";
+import ConstructionVideo from "../../assets/ConstructionVideo.mp4";
+import ThumbnailImage from "../../assets/Thumbnail.svg";
+
+const VideoWithPlayButton = () => {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const videoRef = React.useRef(null);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  return (
+    <div className="video-container">
+      <video
+        ref={videoRef}
+        className={`video-player ${isPlaying ? "visible" : "hidden"}`}
+        src={ConstructionVideo}
+        type="video/mp4"
+        controls
+      ></video>
+
+      {!isPlaying && (
+        <div
+          className="thumbnail-overlay"
+          style={{ backgroundImage: `url(${ThumbnailImage})` }}
+        >
+          <button className="play-button" onClick={handlePlay}>
+            <span>&#9658;</span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VideoWithPlayButton;
