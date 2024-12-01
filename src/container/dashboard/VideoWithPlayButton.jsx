944416464d85d72dca8e0de18b5b27b995e90dd7
Harsh Mishra <harsh@Mishras-MacBook-Air.local>
2024-12-02 00:45:34 +0530

    home page video error fixed

--- src/container/dashboard/VideoWithPlayButton.jsx
+++ src/container/dashboard/VideoWithPlayButton.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import "./VideoWithPlayButton.css";
-import ConstructionVideo from "../../assets/construction.mp4";
 import ThumbnailImage from "../../assets/Thumbnail.svg";
 
 const VideoWithPlayButton = () => {
   const [isPlaying, setIsPlaying] = React.useState(false);
-  const videoRef = React.useRef(null);
 
   const handlePlay = () => {
     setIsPlaying(true);
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
   };
 
   return (
     <div className="video-container">
-      <video
-        ref={videoRef}
-        className={`video-player ${isPlaying ? "visible" : "hidden"}`}
-        src={ConstructionVideo}
-        type="video/mp4"
-        controls
-      ></video>
-
-      {!isPlaying && (
+      {/* YouTube Embed iframe */}
+      {isPlaying ? (
+        <iframe
+          className="video-player visible"
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/WB5u-noMe_M?autoplay=1"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      ) : (
+        // Thumbnail Overlay for the Play Button
         <div
           className="thumbnail-overlay"
           style={{ backgroundImage: `url(${ThumbnailImage})` }}
